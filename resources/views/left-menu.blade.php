7ecc85f09e8050656229065ad2339e40a0511835
unbreakablery <unbreakablery@gmail.com>
2021-06-05 08:07:38 +0900

    updated left-menu

--- resources/views/left-menu.blade.php
+++ resources/views/left-menu.blade.php
@@ -1,60 +1,76 @@
-<nav id="sidebarMenu" class="col-md-3 col-lg-2 d-md-block bg-black sidebar collapse">
-    <div class="sidebar-sticky pt-3 bg-black">
-        <ul class="nav flex-column">
-            <li class="nav-item">
-                <a class="nav-link @if (isset($nl_tasks_class)) {{ $nl_tasks_class }} @endif" href="{{ route('tasks') }}">
-                    <span data-feather="home"></span>
-                    Tasks <span class="sr-only">(current)</span>
-                </a>
-            </li>
-            <li class="nav-item">
-                <a class="nav-link @if (isset($nl_outbound_class)) {{ $nl_outbound_class }} @endif" href="{{ route('outbound') }}">
-                    <span data-feather=""></span>
+<nav class="main-menu">
+    <ul>
+        <li>
+            <a class="nav-link @if (isset($nl_tasks_class)) {{ $nl_tasks_class }} @endif" href="{{ route('tasks') }}">
+                <i class="bi bi-list-task bi-2x"></i>
+                <span class="nav-text">
+                    Tasks
+                </span>
+            </a>
+        </li>
+        <li class="nav-item">
+            <a class="nav-link @if (isset($nl_outbound_class)) {{ $nl_outbound_class }} @endif" href="{{ route('outbound') }}">
+                <i class="bi bi-telephone-outbound-fill bi-2x"></i>    
+                <span class="nav-text">
                     Outbound
-                </a>
-            </li>
-            <li class="nav-item">
-                <a class="nav-link @if (isset($nl_opportunities_class)) {{ $nl_opportunities_class }} @endif" href="{{ route('opportunities') }}">
-                    <span data-feather=""></span>
+                </span>
+            </a>
+        </li>
+        <li class="nav-item">
+            <a class="nav-link @if (isset($nl_opportunities_class)) {{ $nl_opportunities_class }} @endif" href="{{ route('opportunities') }}">
+                <i class="bi bi-signpost-2-fill bi-2x"></i>
+                <span class="nav-text">
                     Opportunities
-                </a>
-            </li>
-            <li class="nav-item">
-                <a class="nav-link @if (isset($nl_scripts_class)) {{ $nl_scripts_class }} @endif" href="{{ route('scripts') }}">
-                    <span data-feather=""></span>
+                </span>
+            </a>
+        </li>
+        <li class="has-subnav">
+            <a class="nav-link @if (isset($nl_scripts_class)) {{ $nl_scripts_class }} @endif" href="{{ route('scripts') }}">
+                <i class="bi bi-terminal-fill bi-2x"></i>
+                <span class="nav-text">
                     Scripts
-                </a>
-            </li>
-            <li class="nav-item">
-                <a class="nav-link @if (isset($nl_emails_class)) {{ $nl_emails_class }} @endif" href="{{ route('emails') }}">
-                    <span data-feather=""></span>
+                </span>
+            </a>
+        </li>
+        <li class="has-subnav">
+            <a class="nav-link @if (isset($nl_emails_class)) {{ $nl_emails_class }} @endif" href="{{ route('emails') }}">
+                <i class="bi bi-envelope-fill bi-2x"></i>
+                <span class="nav-text">
                     Emails
-                </a>
-            </li>
-            <li class="nav-item">
-                <a class="nav-link @if (isset($nl_contacts_class)) {{ $nl_contacts_class }} @endif" href="{{ route('contacts') }}">
-                    <span data-feather=""></span>
+                </span>
+            </a>
+        </li>
+        <li>
+            <a class="nav-link @if (isset($nl_contacts_class)) {{ $nl_contacts_class }} @endif" href="{{ route('contacts') }}">
+                <i class="bi bi-person-lines-fill bi-2x"></i>
+                <span class="nav-text">
                     Contacts
-                </a>
-            </li>
-            <li class="nav-item">
-                <a class="nav-link @if (isset($nl_resources_class)) {{ $nl_resources_class }} @endif" href="{{ route('resources') }}">
-                    <span data-feather=""></span>
+                </span>
+            </a>
+        </li>
+        <li>
+            <a class="nav-link @if (isset($nl_resources_class)) {{ $nl_resources_class }} @endif" href="{{ route('resources') }}">
+                <i class="bi bi-server bi-2x"></i>
+                <span class="nav-text">
                     Resources
-                </a>
-            </li>
-            <li class="nav-item">
-                <a class="nav-link @if (isset($nl_skills_class)) {{ $nl_skills_class }} @endif" href="{{ route('skills') }}">
-                    <span data-feather=""></span>
+                </span>
+            </a>
+        </li>
+        <li>
+           <a class="nav-link @if (isset($nl_skills_class)) {{ $nl_skills_class }} @endif" href="{{ route('skills') }}">
+                <i class="bi bi-table bi-2x"></i>
+                <span class="nav-text">
                     Skills
-                </a>
-            </li>
-            <li class="nav-item">
-                <a class="nav-link @if (isset($nl_analytics_class)) {{ $nl_analytics_class }} @endif" href="{{ route('analytics') }}">
-                    <span data-feather=""></span>
+                </span>
+            </a>
+        </li>
+        <li>
+           <a class="nav-link @if (isset($nl_analytics_class)) {{ $nl_analytics_class }} @endif" href="{{ route('analytics') }}">
+                <i class="bi bi-graph-up bi-2x"></i>
+                <span class="nav-text">
                     Analytics
-                </a>
-            </li>
-        </ul>
-    </div>
+                </span>
+            </a>
+        </li>
+    </ul>
 </nav>
