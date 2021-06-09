71cdb305eb9eb8767f441d89d6fd4dd4e89f21cd
unbreakablery <unbreakablery@gmail.com>
2021-06-10 01:23:44 +0900

    Improve(Database): updated user table for user register and general settings

---
+++ database/migrations/2021_06_09_160215_update_users_table.php
@@ -0,0 +1,47 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class UpdateUsersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('users', function (Blueprint $table) {
+            $table->dropColumn('name');
+            $table->string('first_name');
+            $table->string('last_name');
+            $table->string('phone')->nullable();
+            $table->string('organisation')->nullable();
+            $table->string('region')->nullable();
+            $table->string('industry')->nullable();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('users', function (Blueprint $table) {
+            $table->dropColumn([
+                'first_name',
+                'last_name',
+                'phone',
+                'organisation',
+                'region',
+                'industry'
+            ]);
+            
+            $table->string('name');
+        });
+    }
+}
