bb6ed41de07e6f10ae5bcb5794c7bb9ccd61f998
unbreakablery <unbreakablery@gmail.com>
2021-06-15 22:26:12 +0900

    Feature(Emails): added migration and model for emails main

---
+++ app/Models/EmailMain.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class EmailMain extends Model
+{
+    protected $table = 'emails_main';
+}
---
+++ database/migrations/2021_06_15_132046_create_emails_main_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateEmailsMainTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('emails_main', function (Blueprint $table) {
+            $table->id();
+            $table->integer('user');
+            $table->string('title');
+            $table->string('subject');
+            $table->text('body')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('emails_main');
+    }
+}
