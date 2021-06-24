dda880d2ab5bf79bbdb4aa3d6c7459ebd5755fcc
unbreakablery <unbreakablery@gmail.com>
2021-06-25 08:34:24 +0900

    db dump for skills_settings table

--- db_dump.sql
+++ db_dump.sql
@@ -12,19 +12,19 @@ MySQL - 10.4.17-MariaDB : Database - enterpriseclosing
 /*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
 /*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
 /*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
-/*Table structure for table `skills_assessment` */
+/*Table structure for table `skills_settings` */
 
-DROP TABLE IF EXISTS `skills_assessment`;
+DROP TABLE IF EXISTS `skills_settings`;
 
-CREATE TABLE `skills_assessment` (
+CREATE TABLE `skills_settings` (
   `id` bigint(20) unsigned NOT NULL AUTO_INCREMENT,
-  `s_id` int(11) NOT NULL,
-  `a_date` date NOT NULL,
-  `a_value` double(8,2) NOT NULL DEFAULT 0.00,
+  `user` int(11) NOT NULL,
+  `s_key` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `s_value` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
   `created_at` timestamp NULL DEFAULT NULL,
   `updated_at` timestamp NULL DEFAULT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=161 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
 /*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
 /*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
