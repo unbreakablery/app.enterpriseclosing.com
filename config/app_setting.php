7d9fe6781de21b2a5a1712374de86b1303b4dd31
unbreakablery <unbreakablery@gmail.com>
2021-07-09 18:02:44 +0900

    Feature(Config): added app setting config file

---
+++ config/app_setting.php
@@ -0,0 +1,21 @@
+<?php
+
+return  [
+    'opportunities' => [
+        'input_fields' => [
+            'usecase'           => ['name' => 'Use-Case', 'type' => ''],
+            'emp_num'           => ['name' => 'No. of Employees', 'type' => ''],
+            'close_date'        => ['name' => 'Close Date', 'type' => 'date'],
+            'stage'             => ['name' => 'Stage', 'type' => ''],
+            'next_step'         => ['name' => 'Next Step', 'type' => ''],
+            'amount'            => ['name' => 'Amount', 'type' => ''],
+            'currency'          => ['name' => 'Currency', 'type' => ''],
+            'lead_source'       => ['name' => 'Lead Source', 'type' => ''],
+            'compelling_event'  => ['name' => 'Compelling Event', 'type' => ''],
+            'competition'       => ['name' => 'Competition', 'type' => ''],
+            'what_new_changed'  => ['name' => 'What\'s New / Changed', 'type' => ''],
+            'red_flags'         => ['name' => 'Red Flags', 'type' => ''],
+            'folder_link'       => ['name' => 'Link to Folder', 'type' => '']
+        ]
+    ]
+];
