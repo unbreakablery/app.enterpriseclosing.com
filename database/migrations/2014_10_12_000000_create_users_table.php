db4a8ccd26293605986c8599e87df7a4d9147ead
unbreakablery <unbreakablery@gmail.com>
2021-07-17 00:31:34 +0900

    Fix(Migration): updated migration for user table

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -19,7 +19,7 @@ class CreateUsersTable extends Migration
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
-            $table->enum('role', ['0', '1', '2'])->default('2');
+            $table->enum('role', ['0', '1', '2', '3'])->default('3');
             $table->rememberToken();
             $table->timestamps();
         });
