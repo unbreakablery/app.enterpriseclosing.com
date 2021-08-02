5a0031079a3706545aa99a234e2771face9bc9ed
unbreakablery <unbreakablery@gmail.com>
2021-08-03 06:20:49 +0900

    updated db dumb file

--- db_dump.sql
+++ db_dump.sql
@@ -12,6 +12,35 @@ MySQL - 10.4.17-MariaDB : Database - enterpriseclosing
 /*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
 /*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
 /*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+/*Table structure for table `opportunities_main` */
+
+DROP TABLE IF EXISTS `opportunities_main`;
+
+CREATE TABLE `opportunities_main` (
+  `id` bigint(20) unsigned NOT NULL AUTO_INCREMENT,
+  `user` int(11) NOT NULL,
+  `opportunity` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL,
+  `usecase` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `emp_num` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `close_date` date DEFAULT NULL,
+  `stage` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `next_step` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `amount` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `currency` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `lead_source` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `compelling_event` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `user_num` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `sponsor` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `what_new_changed` text CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `red_flags` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `folder_link` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `competitive_position` tinyint(4) DEFAULT NULL,
+  `progress_barometer` tinyint(4) DEFAULT NULL,
+  `created_at` timestamp NULL DEFAULT NULL,
+  `updated_at` timestamp NULL DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=8 DEFAULT CHARSET=utf8;
+
 /*Table structure for table `opportunities_sales_stage` */
 
 DROP TABLE IF EXISTS `opportunities_sales_stage`;
@@ -20,11 +49,30 @@ CREATE TABLE `opportunities_sales_stage` (
   `id` bigint(20) unsigned NOT NULL AUTO_INCREMENT,
   `opp_id` int(11) NOT NULL,
   `ss_id` int(11) NOT NULL,
-  `strength_indicator` tinyint(4) DEFAULT NULL,
+  `weak` tinyint(4) DEFAULT NULL,
+  `normal` tinyint(4) DEFAULT NULL,
+  `strong` tinyint(4) DEFAULT NULL,
+  `progress` tinyint(4) DEFAULT NULL COMMENT '1: Not Started, 2: In Progress, 3: Completed',
   `created_at` timestamp NULL DEFAULT NULL,
   `updated_at` timestamp NULL DEFAULT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=70 DEFAULT CHARSET=utf8;
+
+/*Table structure for table `opportunities_settings` */
+
+DROP TABLE IF EXISTS `opportunities_settings`;
+
+CREATE TABLE `opportunities_settings` (
+  `id` bigint(20) unsigned NOT NULL AUTO_INCREMENT,
+  `user` int(11) NOT NULL,
+  `o_key` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL,
+  `o_value` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci DEFAULT NULL COMMENT '''input-fields'',''sales-stage''',
+  `o_value1` tinyint(4) DEFAULT NULL COMMENT 'show/hide strength indicator for sales stage',
+  `o_value2` tinyint(4) DEFAULT NULL COMMENT 'order for sales stage',
+  `created_at` timestamp NULL DEFAULT NULL,
+  `updated_at` timestamp NULL DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=24 DEFAULT CHARSET=utf8;
 
 /*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
 /*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
