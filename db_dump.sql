4d22cd87186984b1e2c28d3b62b3fb4a77053281
unbreakablery <unbreakablery@gmail.com>
2021-08-10 06:33:20 +0900

    updated db_dump file

--- db_dump.sql
+++ db_dump.sql
@@ -12,27 +12,53 @@ MySQL - 10.4.17-MariaDB : Database - enterpriseclosing
 /*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
 /*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
 /*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
-/*Table structure for table `opportunities_org_chart` */
+/*Table structure for table `opportunities_jpp_soe` */
 
-DROP TABLE IF EXISTS `opportunities_org_chart`;
+DROP TABLE IF EXISTS `opportunities_jpp_soe`;
 
-CREATE TABLE `opportunities_org_chart` (
+CREATE TABLE `opportunities_jpp_soe` (
   `id` bigint(20) unsigned NOT NULL AUTO_INCREMENT,
   `opp_id` int(11) NOT NULL,
-  `order` int(11) NOT NULL,
-  `first_name` varchar(255) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
-  `last_name` varchar(255) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
-  `title` varchar(255) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
-  `email` varchar(255) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
-  `landline` varchar(255) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
-  `mobile` varchar(255) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
-  `role` tinyint(4) NOT NULL DEFAULT 0 COMMENT '0: '''', 1: ''SPOC'', 2: ''Influencer'', 3: ''Decision Maker''',
-  `engagement` tinyint(4) NOT NULL DEFAULT 0 COMMENT '0: '''', 1: ''Low'', 2: ''Medium'', 3: ''High''',
-  `notes` varchar(255) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `no` int(11) DEFAULT NULL,
+  `task_event` int(11) DEFAULT NULL,
+  `ownership` tinyint(4) NOT NULL DEFAULT 0,
+  `target_date` date DEFAULT NULL,
+  `completed` tinyint(4) NOT NULL DEFAULT 0,
+  `comments` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci DEFAULT NULL,
   `created_at` timestamp NULL DEFAULT NULL,
   `updated_at` timestamp NULL DEFAULT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=25 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+/*Table structure for table `opportunities_main` */
+
+DROP TABLE IF EXISTS `opportunities_main`;
+
+CREATE TABLE `opportunities_main` (
+  `id` bigint(20) unsigned NOT NULL AUTO_INCREMENT,
+  `user` int(11) NOT NULL,
+  `opportunity` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL,
+  `usecase` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `emp_num` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `close_date` date DEFAULT NULL,
+  `stage` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `next_step` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `amount` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `currency` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `lead_source` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `compelling_event` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `user_num` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `sponsor` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `what_new_changed` text CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `red_flags` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `folder_link` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `competitive_position` tinyint(4) DEFAULT NULL,
+  `progress_barometer` tinyint(4) DEFAULT NULL,
+  `opp_organisation` varchar(255) DEFAULT NULL,
+  `created_at` timestamp NULL DEFAULT NULL,
+  `updated_at` timestamp NULL DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=13 DEFAULT CHARSET=utf8;
 
 /*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
 /*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
